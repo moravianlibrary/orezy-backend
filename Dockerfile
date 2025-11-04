3c9575a3a3a6406f73fdc353686832dd6e8c4598
Lucie Novotna <nvtnlucie@gmail.com>
2025-11-04 09:22:45 +0100

    Update deployment

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,17 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 RUN curl -fsSL https://curl.se/ca/cacert.pem -o /usr/local/share/ca-certificates/cacert.pem \
  && update-ca-certificates
 
-COPY . /src/
+COPY app /src/app/
+COPY models /src/models/
+COPY uv.lock* /src/
+COPY pyproject.toml /src/
+
 WORKDIR /src
 # Sync the project into a new environment
 # TODO uncoment after push test
 RUN uv sync --locked
+
+# Create a non-root user
+RUN useradd -m -u 1000 appuser
+RUN mkdir -p /upload_data && chown -R appuser:appuser /upload_data
+USER appuser
--- generate-worker-env.sh
+++ generate-worker-env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Starting database..."
-docker compose up -d hatchet-lite postgres
+docker compose up -d setup-config postgres --remove-orphans
 
 echo "Waiting for Hatchet server to be ready..."
 sleep 10
 
 : > .worker-env
 cat <<EOF > .worker-env
-HATCHET_CLIENT_TOKEN="$(docker compose -f docker-compose.yml exec hatchet-lite /hatchet-admin token create --config /config --tenant-id 707d0855-80ab-4e1f-a156-f1c4546cbf52 | xargs)"
+HATCHET_CLIENT_TOKEN=$(docker compose -f docker-compose.yml run --no-deps setup-config /hatchet/hatchet-admin token create --config /hatchet/config --tenant-id 707d0855-80ab-4e1f-a156-f1c4546cbf52 | xargs)
 HATCHET_CLIENT_TLS_STRATEGY=none
 EOF
 echo "Worker environment variables written to .worker-env"
